0d226f5b1ebf0a643024b8cc721093ccc48a3824
Eric <eric_h_y@hotmail.com>
2018-12-02 18:37:33 -0200

    Altera params do rádio lora

--- Lora-Tiva/main.h
+++ Lora-Tiva/main.h
@@ -41,8 +41,8 @@
                                                                   //  1: 250 kHz,
                                                                   //  2: 500 kHz,
                                                                   //  3: Reserved]
-    #define LORA_SPREADING_FACTOR                       12        // [SF7..SF12]
-    #define LORA_CODINGRATE                             4         // [1: 4/5,
+    #define LORA_SPREADING_FACTOR                       9         // [SF7..SF12]
+    #define LORA_CODINGRATE                             2         // [1: 4/5,
                                                                   //  2: 4/6,
                                                                   //  3: 4/7,
                                                                   //  4: 4/8]
